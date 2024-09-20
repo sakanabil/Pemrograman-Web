47e8bd83021fc5b322e4688b17f4e66a17f17ca3
saka <sakanabil1221@gmail.com>
2024-09-20 07:50:05 +0700

    Soal 3.1

---
+++ Pertemuan 4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil tambah: {$hasilTambah} <br>";
+echo "Hasil kurang: {$hasilKurang} <br>";
+echo "Hasil kali: {$hasilKali} <br>";
+echo "Hasil bagi: {$hasilBagi} <br>";
+echo "Hasil sisa bagi: {$sisaBagi} <br>";
+echo "Hasil pangkat: {$pangkat} <br>";
+?>
