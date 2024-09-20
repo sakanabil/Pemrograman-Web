ff80633ffbf2573f23d5c64b443641b5f4914afe
saka <sakanabil1221@gmail.com>
2024-09-20 08:55:31 +0700

    Soal 4.4

--- Pertemuan 4/struktur_kontrol.php
+++ Pertemuan 4/struktur_kontrol.php
@@ -36,4 +36,14 @@ for ($i = 1; $i <= $jumlahLahan; $i++) {
 }
 
 echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+echo "<br>";
+
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah: $totalSkor"
 ?>
