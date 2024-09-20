5fde405d2d234fac3a3baa58b43bdfd0e17f32e7
saka <sakanabil1221@gmail.com>
2024-09-20 09:20:43 +0700

    Soal 4.8

--- Pertemuan 4/struktur_kontrol.php
+++ Pertemuan 4/struktur_kontrol.php
@@ -79,4 +79,13 @@ if ($hargaBeli > 100000) {
 }
 
 echo "Harga setelah diskon: " . $hargaSetelahDiskon;
+echo "<br>";
+
+$poin = 600;
+
+$totalSkor = $poin;
+$hadiahTambahan = ($poin > 500) ? 'YA' : 'TIDAK';
+
+echo "Total skor pemain adalah: $totalSkor <br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiahTambahan <br>";
 ?>
