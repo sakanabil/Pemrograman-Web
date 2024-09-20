65042e1c49978a46a410f0fd704382625ed3f156
saka <sakanabil1221@gmail.com>
2024-09-20 08:09:15 +0700

    Soal 3.2

--- Pertemuan 4/operator.php
+++ Pertemuan 4/operator.php
@@ -8,6 +8,13 @@ $hasilKali = $a * $b;
 $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
 
 echo "Hasil tambah: {$hasilTambah} <br>";
 echo "Hasil kurang: {$hasilKurang} <br>";
@@ -15,4 +22,10 @@ echo "Hasil kali: {$hasilKali} <br>";
 echo "Hasil bagi: {$hasilBagi} <br>";
 echo "Hasil sisa bagi: {$sisaBagi} <br>";
 echo "Hasil pangkat: {$pangkat} <br>";
+echo "Apakah $a sama dengan $b? : " . ($hasilSama ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah $a tidak sama dengan $b? : " . ($hasilTidakSama ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah $a lebih kecil dari $b? : " . ($hasilLebihKecil ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah $a lebih besar dari $b? : " . ($hasilLebihBesar ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah $a lebih kecil atau sama dengan $b? : " . ($hasilLebihKecilSama ? 'Ya' : 'Tidak') . "<br>";
+echo "Apakah $a lebih besar atau sama dengan $b? : " . ($hasilLebihBesarSama ? 'Ya' : 'Tidak') . "<br>";
 ?>
