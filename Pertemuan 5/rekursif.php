d96db411908adbb8a49c71498cc43c82ac886c44
saka <sakanabil1221@gmail.com>
2024-09-27 08:12:24 +0700

    Soal 6.2

--- Pertemuan 5/rekursif.php
+++ Pertemuan 5/rekursif.php
@@ -1,9 +1,12 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo Dunia! <br>";
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
 
-    tampilkanHaloDunia();
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
 
-tampilkanHaloDunia();
+tampilkanAngka(20);
+
 ?>
