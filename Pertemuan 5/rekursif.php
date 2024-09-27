a04c54e443c34df50f1abe4a12a483df277d6464
saka <sakanabil1221@gmail.com>
2024-09-27 08:21:13 +0700

    Soal 7

--- Pertemuan 5/rekursif.php
+++ Pertemuan 5/rekursif.php
@@ -8,5 +8,52 @@ function tampilkanAngka (int $jumlah, int $indeks = 1) {
 }
 
 tampilkanAngka(20);
+?>
 
+<hr>
+
+<?php
+$menu = [
+    [
+        "nama" => "Beranda",
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai",
+                    ],
+                    [
+                        "nama" => "Gunung",
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner",
+            ],
+            [
+                "nama" => "Hiburan",
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang",
+    ],
+    [
+        "nama" => "Kontak",
+    ],
+];
+
+function tampilkanMenuBertingkat (array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item["nama"]}</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
 ?>
