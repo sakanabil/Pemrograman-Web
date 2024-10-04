368fd120be83af01108ff2a0daf4a53826aa22c4
saka <sakanabil1221@gmail.com>
2024-10-04 15:42:09 +0700

    Revisi Soal 17 lagi

--- Pertemuan 6/cara1.php
+++ Pertemuan 6/cara1.php
@@ -7,3 +7,12 @@
         <p>Tanggal Hari ini : <?php echo date("d M Y")?></p>
     </body>
 </html>
+
+<?php
+echo '<html>';
+echo '<head><title>Cara 02</title></head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini : ' . date("d M Y") . '</p>';
+echo '</body>';
+echo '</html>';
+?>
