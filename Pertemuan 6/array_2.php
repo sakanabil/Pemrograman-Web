648cab29bb3e84a75549fbb09bef47db0e8483b3
saka <sakanabil1221@gmail.com>
2024-10-04 08:58:55 +0700

    Soal 2

--- Pertemuan 6/array_2.php
+++ Pertemuan 6/array_2.php
@@ -4,17 +4,43 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title> 
+        <style>
+            table {
+                border-collapse: collapse;
+                border-spacing: 0;
+                width: 100%;
+                border: 2px solid #ddd;
+            }
+
+            th, td {
+                text-align: left;
+                padding: 18px;
+            }
+
+            tr:nth-child(even) {
+                background-color: #f1f1f1;
+            }
+        </style>
     </head>
     <body>
+        <table>
+            <tr>
+                <th> Nama </th>
+                <th> Domisili </th>
+                <th> Jenis Kelamin </th>
+            </tr>
         <?php
-        $Dosen = [
-            'nama' => 'Elok Nur Hamdana',
-            'domisili' => 'Malang',
-            'jenis_kelamin' => 'Perempuan' ];
-            
-            echo "Nama : {$Dosen['nama']} <br>";
-            echo "Domisili : {$Dosen['domisili']} <br>";
-            echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";  
+        $Dosen = array(
+            array("Elok Nur Hamdana", "Malang", "Perempuan"),
+        );
+        echo "<tr>";
+        foreach ($Dosen as $dosen) {
+            echo "<td>". $dosen[0] ."</td>";
+            echo "<td>". $dosen[1] ."</td>";
+            echo "<td>". $dosen[2] ."</td>";
+        }
+        echo "</tr>";
         ?> 
+        </table>
     </body>
 </html>
