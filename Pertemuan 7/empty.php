586d71835989bff5ee29a4d11cf5eceb1710f0d9
saka <sakanabil1221@gmail.com>
2024-10-08 19:39:40 +0700

    Soal 2.2

--- Pertemuan 7/empty.php
+++ Pertemuan 7/empty.php
@@ -1,8 +1,14 @@
 <?php
 $myArray = array();
 if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong";
+    echo "Array tidak terdefinisi atau kosong" . "<br>";
 } else {
-    echo "Array terdefinisi dan tidak kosong";
+    echo "Array terdefinisi dan tidak kosong" . "<br>";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong" . "<br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong" . "<br>";
 }
 ?>
