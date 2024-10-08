a8bb8cb702244fdaa23bcc1c398dab67a87416c8
saka <sakanabil1221@gmail.com>
2024-10-08 21:09:55 +0700

    Soal 7.2

--- Pertemuan 7/form_validasi.php
+++ Pertemuan 7/form_validasi.php
@@ -2,19 +2,50 @@
 <html>
     <head>
         <title>Form Input dengan Validasi</title>
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     </head>
     <body>
         <h1>Form Input dengan Validasi</h1>
-        <form method="POST" action="proses_validasi.php">
+        <form id="myForm" method="POST" action="proses_validasi.php">
             <label for="nama">Nama:</label>
             <input type="text" id="nama" name="nama">
+            <span id="nama-error" style="color: red;"></span>
             <br>
 
             <label for="email">Email:</label>
             <input type="email" id="email" name="email">
+            <span id="email-error" style="color: red;"></span>
             <br>
 
             <input type="submit" value="Submit">
         </form>
+
+        <script>
+            $(document).ready(function() {
+                $('#myForm').submit(function(event) {
+                    var nama = $('#nama').val();
+                    var email = $('#email').val();
+                    var valid = true;
+
+                    if (nama === "") {
+                        $('#nama-error').text('Nama harus diisi.');
+                        valid = false;
+                    } else {
+                        $('#nama-error').text("");
+                    }
+
+                    if (email === "") {
+                        $('#email-error').text('Email harus diisi.');
+                        valid = false;
+                    } else {
+                        $('#email-error').text("");
+                    }
+
+                    if (!valid) {
+                        event.preventDefault();
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
