7e7d69f16ed9d09a17a5a041705b67ed098f1a25
saka <sakanabil1221@gmail.com>
2024-10-08 20:14:15 +0700

    Soal 5.2

--- Pertemuan 7/regex.php
+++ Pertemuan 7/regex.php
@@ -6,4 +6,14 @@ if (preg_match($pattern, $text)) {
 } else {
     echo "Tidak ada huruf kecil!";
 }
+
+echo "<br>";
+
+$pattern = '/[0-9]/';
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
